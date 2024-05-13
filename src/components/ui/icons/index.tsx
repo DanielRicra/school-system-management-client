7debb320dfb0af98e5eb5fbca8e1d1dca964009e
DanielRicra <daniel.int@outlook.com>
2024-05-13 08:47:28 -0500

    Export the icons in components ui folder
    Icons components: StudentIcon, ClassroomIcon, RoomsIcon and TeachersIcon

---
+++ src/components/ui/icons/index.tsx
@@ -0,0 +1,6 @@
+import StudentsIcon from "@/assets/icons/students.svg?react";
+import ClassroomsIcon from "@/assets/icons/classrooms.svg?react";
+import RoomsIcon from "@/assets/icons/rooms.svg?react";
+import TeachersIcon from "@/assets/icons/teachers.svg?react";
+
+export { StudentsIcon, ClassroomsIcon, RoomsIcon, TeachersIcon };
