cb2d6b03d78bcd386c26b3242c6baa7c8d5e085b
DanielRicra <daniel.int@outlook.com>
2024-05-13 23:01:15 -0500

    Add createPaginationLinks to lib utils
    This function is for create the links to render in data-pagination component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,32 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function createPaginationLinks(
+  total: number,
+  active: number
+): Array<number | null> {
+  if (total < 1) return [];
+
+  if (total <= 5 && total >= 1) {
+    return Array.from({ length: total }, (_, i) => i + 1);
+  }
+
+  if (active < 3) {
+    return [1, 2, 3, null, total];
+  }
+
+  if (active === 3) {
+    return [1, 2, 3, 4, null, total];
+  }
+
+  const halfLeft = active - 1;
+
+  if (halfLeft >= total - 3) {
+    return [1, null, total - 3, total - 2, total - 1, total];
+  }
+
+  const halfLinks = Array.from({ length: 3 }, (_, i) => halfLeft + i);
+
+  return [1, null, ...halfLinks, null, total];
+}
