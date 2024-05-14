a5d7a060790c2b2a52edf47aaa56982ee5f7e85e
DanielRicra <daniel.int@outlook.com>
2024-05-13 23:07:09 -0500

    Add AdminStudents component to /admin/students
    Introduces StudentsTable and DataPagination

--- src/routes/_admin/admin/students/index.lazy.tsx
+++ src/routes/_admin/admin/students/index.lazy.tsx
@@ -1,5 +1,54 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from "@tanstack/react-router";
 
-export const Route = createLazyFileRoute('/_admin/admin/students/')({
-  component: () => <div>Hello Students!</div>
-})
+import { TypographyH2 } from "@/components/ui/typography";
+import { DataPagination } from "@/components/ui/data-pagination";
+import { StudentsTable } from "./-components/data-table";
+
+export const Route = createLazyFileRoute("/_admin/admin/students/")({
+  component: AdminStudents,
+});
+
+const data = [
+  {
+    id: "4587-abc-654-xzy-uuid",
+    gradeLevel: "5th",
+    classroomId: 4,
+    userId: "4587-abc-654-xzy-uuid",
+    firstName: "Gandalf",
+    surname: "The gray",
+    userCode: "123A56",
+    enrollmentStatus: "active",
+    createdAt: "January 1, 2002",
+  },
+];
+
+function AdminStudents() {
+  const { page: activePage = 1 } = Route.useSearch();
+  const lastPage = 10;
+
+  return (
+    <main className="p-8 flex flex-col gap-4 items-start">
+      <div className="mb-3 flex flex-col items-start">
+        <TypographyH2>Students</TypographyH2>
+        <p className="text-lg text-muted-foreground mt-2">
+          Manage the students.
+        </p>
+        <p className="text-lg leading-tight text-muted-foreground mt-3">
+          Total: <span className="font-bold">1500</span>
+        </p>
+      </div>
+
+      <div className="my-3 rounded-md border">
+        <StudentsTable data={data} />
+      </div>
+
+      {data.length > 0 ? (
+        <DataPagination
+          active={activePage}
+          total={lastPage}
+          pathRoute="/admin/students"
+        />
+      ) : null}
+    </main>
+  );
+}
