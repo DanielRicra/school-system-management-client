bcf3882a3388cc1091e819c15f8ba47dcdcfcb1d
DanielRicra <daniel.int@outlook.com>
2024-05-15 19:19:03 -0500

    Add useFetchStudents hook
    For fetching student data with SWR
    
    - Accepts an optional query object for building query strings.
    - Uses qs library to stringify the query object.
    - Employs useSWR hook with a dynamic key based on the constructed URL.
    - Calls apiService.get to fetch data and wraps it in request for SWR handling.

---
+++ src/hooks/http-requests/use-fetch-students.ts
@@ -0,0 +1,32 @@
+import qs from "query-string";
+import useSWR from "swr";
+
+import apiService, { type HttpServiceError } from "@/services/api";
+import type { QueryString } from "@/types";
+
+interface FetchStudentsConfig {
+  query: QueryString;
+}
+
+function useFetchStudents<T>({ query }: FetchStudentsConfig) {
+  const abortController = new AbortController();
+
+  const queryString = qs.stringify(query);
+  return useSWR<T, HttpServiceError>(
+    `/student?${queryString}`,
+    (url: string) => apiService.get(url, undefined, abortController.signal),
+    {
+      onErrorRetry: (error, _key, _config, revalidate, { retryCount }) => {
+        const { status } = error;
+        if (status === 404 || status === 400 || status === 403) return;
+
+        if (retryCount >= 3) return;
+
+        setTimeout(() => revalidate({ retryCount }), 5000);
+      },
+      revalidateOnFocus: false,
+    }
+  );
+}
+
+export default useFetchStudents;
