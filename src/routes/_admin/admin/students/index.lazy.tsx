e05a34c033b800a3a94e2c306917371b9e25d382
DanielRicra <daniel.int@outlook.com>
2024-05-15 21:40:23 -0500

    Integrate useFetchStudents hook
    For dynamic student data & pagination
    
    - Replaces mock student data with useFetchStudents hook for live data fetching.
    - Constructs a query object with page and ordering parameters for the API call.
    - Handles loading, error states, and displays appropriate messages or data.

--- src/routes/_admin/admin/students/index.lazy.tsx
+++ src/routes/_admin/admin/students/index.lazy.tsx
@@ -1,57 +1,72 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
 
 import { TypographyH2 } from "@/components/ui/typography";
 import { DataPagination } from "@/components/ui/data-pagination";
 import { StudentsTable } from "./-components/data-table";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { useFetchStudents } from "@/hooks/http-requests";
+import type { ListResponse, Student } from "@/services/types";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const Route = createLazyFileRoute("/_admin/admin/students/")({
   component: AdminStudents,
 });
 
-const data = [
-  {
-    id: "4587-abc-654-xzy-uuid",
-    gradeLevel: "5th",
-    classroomId: 4,
-    userId: "4587-abc-654-xzy-uuid",
-    firstName: "Gandalf",
-    surname: "The gray",
-    userCode: "123A56",
-    enrollmentStatus: "active",
-    createdAt: "January 1, 2002",
-    updatedAt: "January 1, 2002",
-  },
-];
-
 function AdminStudents() {
-  const { page: activePage = 1 } = Route.useSearch();
-  const lastPage = 10;
+  const { page: activePage = 1, ordering } = Route.useSearch();
+
+  const {
+    data: students,
+    isLoading,
+    error,
+  } = useFetchStudents<ListResponse<Student>>({
+    query: { page: activePage, ordering },
+  });
+
+  console.log(isLoading);
 
   return (
-    <main className="flex flex-col gap-4 py-8 items-start container h-full w-full">
-      <div className="mb-3 flex flex-col items-start">
-        <TypographyH2>Students</TypographyH2>
-        <p className="text-lg text-muted-foreground mt-2">
-          Manage the students.
-        </p>
-        <p className="text-lg leading-tight text-muted-foreground mt-3">
-          Total: <span className="font-bold">1500</span>
-        </p>
-      </div>
+    <ScrollArea className="h-full w-full">
+      <main className="flex flex-col gap-4 py-8 items-start container h-full w-full">
+        <div className="mb-3 flex flex-col items-start">
+          <TypographyH2>
+            Students -{" "}
+            <span className="font-bold">({students?.info.count ?? 0})</span>
+          </TypographyH2>
+          <p className="text-muted-foreground mt-2">Manage the students.</p>
+        </div>
 
-      <ScrollArea className="my-3 rounded-md border w-full overflow-x-auto lg:max-w-[1000px] md:max-w-[700px]">
-        <StudentsTable data={data} />
-        <ScrollBar orientation="horizontal" />
-      </ScrollArea>
+        {isLoading ? (
+          <div className="space-y-3 bg-red w-full">
+            <Skeleton className="h-[325px] w-full bg-secondary" />
+          </div>
+        ) : students ? (
+          <div className="rounded-md my-3 border w-full lg:max-w-[1000px] md:max-w-[700px]">
+            <ScrollArea>
+              <StudentsTable data={students.results} />
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+          </div>
+        ) : null}
 
-      {data.length > 0 ? (
-        <DataPagination
-          active={activePage}
-          total={lastPage}
-          pathRoute="/admin/students"
-        />
-      ) : null}
-    </main>
+        {error ? (
+          <Alert variant="destructive">
+            <ExclamationTriangleIcon className="h-4 w-4" />
+            <AlertTitle className="font-bold">Error</AlertTitle>
+            <AlertDescription>{error.message}</AlertDescription>
+          </Alert>
+        ) : null}
+
+        {students && students.results.length > 0 ? (
+          <DataPagination
+            active={activePage}
+            total={students.info.lastPage}
+            pathRoute="/admin/students"
+          />
+        ) : null}
+      </main>
+    </ScrollArea>
   );
 }
