05b8ed10acbd7aa2e5391a4b6a36e3dd98721adc
DanielRicra <daniel.int@outlook.com>
2024-05-16 22:07:55 -0500

    Add per_page to studentSearchSchema
    In admin/students index.tsx file

--- src/routes/_admin/admin/students/index.tsx
+++ src/routes/_admin/admin/students/index.tsx
@@ -1,11 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 import * as v from "valibot";
 
-const productSearchSchema = v.object({
+const studentSearchSchema = v.object({
   page: v.fallback(v.optional(v.number([v.minValue(1)])), 1),
   ordering: v.fallback(v.optional(v.string([])), ""),
+  per_page: v.fallback(v.optional(v.number([v.minValue(10)])), 10),
 });
 
 export const Route = createFileRoute("/_admin/admin/students/")({
-  validateSearch: (search) => v.parse(productSearchSchema, search),
+  validateSearch: (search) => v.parse(studentSearchSchema, search),
 });
