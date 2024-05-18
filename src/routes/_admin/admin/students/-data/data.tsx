899f096aeb54516c4f926c3cec3bc1b12d5aacef
DanielRicra <daniel.int@outlook.com>
2024-05-17 22:03:13 -0500

    add student data.tsx to constants
    Has:
    - enrollmentStatuses, a list of enrollment status options with value, label an icon.
    - gradeLevels, a list with label and value objects

---
+++ src/routes/_admin/admin/students/-data/data.tsx
@@ -0,0 +1,48 @@
+import { CapIcon } from "@/components/ui/icons";
+import { CheckIcon, LockClosedIcon, UpdateIcon } from "@radix-ui/react-icons";
+
+export const enrollmentStatuses = [
+  {
+    value: "active",
+    label: "Active",
+    icon: CheckIcon,
+  },
+  {
+    value: "graduated",
+    label: "Graduated",
+    icon: CapIcon,
+  },
+  {
+    value: "inactive",
+    label: "Inactive",
+    icon: LockClosedIcon,
+  },
+  {
+    value: "transferred",
+    label: "Transferred",
+    icon: UpdateIcon,
+  },
+];
+
+export const gradeLevels = [
+  {
+    label: "1st",
+    value: "1st",
+  },
+  {
+    label: "2nd",
+    value: "2nd",
+  },
+  {
+    label: "3rd",
+    value: "3rd",
+  },
+  {
+    label: "4th",
+    value: "4th",
+  },
+  {
+    label: "5th",
+    value: "5th",
+  },
+];
