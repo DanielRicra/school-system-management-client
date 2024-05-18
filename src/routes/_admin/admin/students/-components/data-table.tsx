f325ec077ee0c845565fcff6301d71d06d017426
DanielRicra <daniel.int@outlook.com>
2024-05-17 22:04:16 -0500

    Add DataTableToolbar and DataTablePagination
    - Add the scroll are for the table

--- src/routes/_admin/admin/students/-components/data-table.tsx
+++ src/routes/_admin/admin/students/-components/data-table.tsx
@@ -1,4 +1,4 @@
-import { DotsHorizontalIcon, CaretSortIcon } from "@radix-ui/react-icons";
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
 import { Link } from "@tanstack/react-router";
 import { format } from "date-fns";
 
@@ -24,9 +24,15 @@ import {
 import { cn } from "@/lib/utils";
 import type { Student } from "@/services/types";
 import { DataTableColumnHeader } from "@/components/ui/data-table-column-header";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { DataTablePagination } from "@/components/ui/data-table-pagination";
+import { DataTableToolbar } from "./data-table-toolbar";
 
-interface StudentsTableProps {
+interface StudentsDataTableProps {
   data: Student[];
+  activePage: number;
+  lastPage: number;
+  perPage?: number;
 }
 
 const columns = [
@@ -42,82 +48,110 @@ const columns = [
   { header: "Actions", ordering: null },
 ];
 
-export function StudentsTable({ data }: StudentsTableProps) {
+export function StudentsDataTable({
+  data,
+  activePage,
+  lastPage,
+  perPage,
+}: StudentsDataTableProps) {
   return (
-    <Table>
-      <TableCaption>A list of all the students.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          {columns.map(({ ordering, header }) => (
-            <TableHead key={header}>
-              <DataTableColumnHeader ordering={ordering} title={header} />
-            </TableHead>
-          ))}
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.length > 0 ? (
-          data.map((student) => (
-            <TableRow key={student.id} className="*:px-2">
-              <TableCell>{student.gradeLevel}</TableCell>
-              <TableCell>{student.classroomId}</TableCell>
-              <TableCell>
-                <Link
-                  to="/admin/users/$userId"
-                  params={{ userId: student.userId }}
-                  className={cn(
-                    buttonVariants({ variant: "link" }),
-                    "p-0 text-secondary-foreground"
-                  )}
-                >
-                  <span className="max-w-[80px] text-nowrap truncate">
-                    {student.userId}
-                  </span>
-                </Link>
-              </TableCell>
-              <TableCell>{student.user?.firstName ?? "--"}</TableCell>
-              <TableCell>{student.user?.surname ?? "--"}</TableCell>
-              <TableCell>{student.user?.code ?? "--"}</TableCell>
-              <TableCell className="text-center">
-                <Badge variant="secondary">{student.enrollmentStatus}</Badge>
-              </TableCell>
-              <TableCell>
-                {format(new Date(student.createdAt), "cccccc, PPp")}
-              </TableCell>
-              <TableCell>
-                {format(new Date(student.updatedAt), "cccccc, PPp")}
-              </TableCell>
-              <TableCell>
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="h-8 w-8 p-0">
-                      <span className="sr-only">Open menu</span>
-                      <DotsHorizontalIcon className="h-4 w-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                    <DropdownMenuItem
-                      onClick={() => navigator.clipboard.writeText(student.id)}
-                    >
-                      Copy Student ID
-                    </DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>Edit Student</DropdownMenuItem>
-                    <DropdownMenuItem>Delete Student</DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
-            </TableRow>
-          ))
-        ) : (
-          <TableRow>
-            <TableCell colSpan={columns.length} className="text-center h-24">
-              No results found.
-            </TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
+    <>
+      <DataTableToolbar />
+
+      <div className="rounded-md my-3 border w-full lg:max-w-[1000px] md:max-w-[700px]">
+        <ScrollArea>
+          <Table>
+            <TableCaption>A list of all the students.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                {columns.map(({ ordering, header }) => (
+                  <TableHead key={header}>
+                    <DataTableColumnHeader ordering={ordering} title={header} />
+                  </TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {data.length > 0 ? (
+                data.map((student) => (
+                  <TableRow key={student.id} className="*:px-2">
+                    <TableCell>{student.gradeLevel}</TableCell>
+                    <TableCell>{student.classroomId}</TableCell>
+                    <TableCell>
+                      <Link
+                        to="/admin/users/$userId"
+                        params={{ userId: student.userId }}
+                        className={cn(
+                          buttonVariants({ variant: "link" }),
+                          "p-0 text-secondary-foreground"
+                        )}
+                      >
+                        <span className="max-w-[80px] text-nowrap truncate">
+                          {student.userId}
+                        </span>
+                      </Link>
+                    </TableCell>
+                    <TableCell>{student.user?.firstName ?? "--"}</TableCell>
+                    <TableCell>{student.user?.surname ?? "--"}</TableCell>
+                    <TableCell>{student.user?.code ?? "--"}</TableCell>
+                    <TableCell className="text-center">
+                      <Badge variant="secondary">
+                        {student.enrollmentStatus}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>
+                      {format(new Date(student.createdAt), "cccccc, PPp")}
+                    </TableCell>
+                    <TableCell>
+                      {format(new Date(student.updatedAt), "cccccc, PPp")}
+                    </TableCell>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <DotsHorizontalIcon className="h-4 w-4" />
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          <DropdownMenuItem
+                            onClick={() =>
+                              navigator.clipboard.writeText(student.id)
+                            }
+                          >
+                            Copy Student ID
+                          </DropdownMenuItem>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem>Edit Student</DropdownMenuItem>
+                          <DropdownMenuItem>Delete Student</DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="text-center h-24"
+                  >
+                    No results found.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+      </div>
+
+      <DataTablePagination
+        active={activePage}
+        total={lastPage}
+        pathRoute="/admin/students"
+        perPage={perPage}
+      />
+    </>
   );
 }
