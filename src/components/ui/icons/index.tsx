150a0249137f693adbad7990f2ab50a4c867065c
DanielRicra <daniel.int@outlook.com>
2024-05-17 21:49:48 -0500

    Add cap svg icon, and exported as react component

--- src/components/ui/icons/index.tsx
+++ src/components/ui/icons/index.tsx
@@ -2,5 +2,6 @@ import StudentsIcon from "@/assets/icons/students.svg?react";
 import ClassroomsIcon from "@/assets/icons/classrooms.svg?react";
 import RoomsIcon from "@/assets/icons/rooms.svg?react";
 import TeachersIcon from "@/assets/icons/teachers.svg?react";
+import CapIcon from "@/assets/icons/cap.svg?react";
 
-export { StudentsIcon, ClassroomsIcon, RoomsIcon, TeachersIcon };
+export { StudentsIcon, ClassroomsIcon, RoomsIcon, TeachersIcon, CapIcon };
