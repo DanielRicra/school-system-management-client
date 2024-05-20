e22d2ace61a0ca8c50b036223156e448d3df2c54
DanielRicra <daniel.int@outlook.com>
2024-05-20 12:12:46 -0500

    Move and rename students/-schema to src/schemas/search-schemas.ts

--- src/routes/_admin/admin/students/index.tsx
+++ src/routes/_admin/admin/students/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { parse } from "valibot";
 
-import { studentSearchSchema } from "./-schemas";
+import { studentSearchSchema } from "@/schemas/search-schemas";
 
 export const Route = createFileRoute("/_admin/admin/students/")({
   validateSearch: (search) => parse(studentSearchSchema, search),
