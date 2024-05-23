f0a206ba3e58106f4e93880acf349c4373c79bd0
DanielRicra <daniel.int@outlook.com>
2024-05-23 16:55:13 -0500

    Enhance with add button and sheet integration
    - Implements a Button component with PlusIcon for adding a new student.
    - Defines onOpen handler using useStudentsSheet hook to trigger sheet opening.
    -Renders the StudentsSheetProvider component for centralized student sheet management.
    - Adds visual styling for the button and potential error alert in dark mode.

--- src/routes/_admin/admin/students/index.lazy.tsx
+++ src/routes/_admin/admin/students/index.lazy.tsx
@@ -1,5 +1,5 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import { ExclamationTriangleIcon, PlusIcon } from "@radix-ui/react-icons";
 
 import { TypographyH2 } from "@/components/ui/typography";
 import { StudentsDataTable } from "./-components/data-table";
@@ -8,6 +8,9 @@ import { useFetchStudents } from "@/hooks/http-requests";
 import type { ListResponse, Student } from "@/services/types";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Button } from "@/components/ui/button";
+import { StudentsSheetProvider } from "./-components/students-sheet-provider";
+import { useStudentsSheet } from "@/hooks/use-new-student";
 
 export const Route = createLazyFileRoute("/_admin/admin/students/")({
   component: AdminStudents,
@@ -24,14 +27,26 @@ function AdminStudents() {
     query: searchQuery,
   });
 
+  const onOpen = useStudentsSheet((state) => state.onOpen);
+
   return (
     <ScrollArea className="h-full w-full">
       <main className="flex flex-col gap-4 py-8 items-start container h-full w-full">
-        <div className="mb-3 flex flex-col items-start">
-          <TypographyH2>
-            Students -{" "}
-            <span className="font-bold">({students?.info?.count ?? 0})</span>
-          </TypographyH2>
+        <div className="mb-3 flex flex-col items-start w-full lg:max-w-[1000px] md:max-w-[700px]">
+          <div className="flex w-full justify-between">
+            <TypographyH2>
+              Students -{" "}
+              <span className="font-bold">({students?.info?.count ?? 0})</span>
+            </TypographyH2>
+            <Button
+              type="button"
+              className="flex gap-1 font-semibold"
+              onClick={() => onOpen("create")}
+            >
+              Add
+              <PlusIcon className="h-4 w-4" />
+            </Button>
+          </div>
           <p className="text-muted-foreground mt-2">Manage the students.</p>
         </div>
 
@@ -52,13 +67,18 @@ function AdminStudents() {
         ) : null}
 
         {!students && !isLoading && error ? (
-          <Alert variant="destructive">
-            <ExclamationTriangleIcon className="h-4 w-4" />
+          <Alert
+            variant="destructive"
+            className="dark:text-red-500 dark:border-red-500 max-w-[400px]"
+          >
+            <ExclamationTriangleIcon className="h-4 w-4 dark:text-red-500" />
             <AlertTitle className="font-bold">Error</AlertTitle>
             <AlertDescription>{error.message}</AlertDescription>
           </Alert>
         ) : null}
       </main>
+
+      <StudentsSheetProvider />
     </ScrollArea>
   );
 }
