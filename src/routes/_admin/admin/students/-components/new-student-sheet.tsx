b87dc093c4f75f08f03e218fb2a387a360723714
DanielRicra <daniel.int@outlook.com>
2024-05-23 16:47:06 -0500

    Add NewStudentSheet component
    To open when the admin wants to add new student

---
+++ src/routes/_admin/admin/students/-components/new-student-sheet.tsx
@@ -0,0 +1,31 @@
+import { useStudentsSheet } from "@/hooks/use-new-student";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetDescription,
+} from "@/components/ui/sheet";
+import { AddStudentForm } from "./add-student-form";
+
+export function NewStudentSheet() {
+  const isOpen = useStudentsSheet((state) => state.isOpen);
+  const type = useStudentsSheet((state) => state.type);
+  const onClose = useStudentsSheet((state) => state.onClose);
+
+  const isSheetOpen = isOpen && type === "create";
+
+  return (
+    <Sheet open={isSheetOpen} onOpenChange={onClose}>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Add new student</SheetTitle>
+          <SheetDescription>Add a new student</SheetDescription>
+        </SheetHeader>
+
+        <AddStudentForm />
+      </SheetContent>
+    </Sheet>
+  );
+}
