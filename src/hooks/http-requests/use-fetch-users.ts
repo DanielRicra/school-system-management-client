a3692ff8e8d422e2c3da769a4eb859d84226ca4f
DanielRicra <daniel.int@outlook.com>
2024-05-24 20:06:13 -0500

    Add useFetchUsersWithoutStudent hook
    - To retrieve users that have no student associate.

---
+++ src/hooks/http-requests/use-fetch-users.ts
@@ -0,0 +1,13 @@
+import qs from "query-string";
+import useSWR from "swr";
+
+import apiService, { type HttpServiceError } from "@/services/api";
+
+export function useFetchUsersWithoutStudent<T>(fullName?: string) {
+  const queryString = qs.stringify({ full_name: fullName });
+
+  return useSWR<T, HttpServiceError>(
+    `/users/without-student?${queryString}`,
+    (url: string) => apiService.get(url)
+  );
+}
