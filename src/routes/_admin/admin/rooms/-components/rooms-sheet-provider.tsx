592697259a81b4711de6ab8506c0f4c4df32d139
DanielRicra <daniel.int@outlook.com>
2024-05-29 19:25:27 -0500

    Add EditRoomSheet into RoomSheetProvider

--- src/routes/_admin/admin/rooms/-components/rooms-sheet-provider.tsx
+++ src/routes/_admin/admin/rooms/-components/rooms-sheet-provider.tsx
@@ -1,9 +1,11 @@
+import { EditRoomSheet } from "./edit-room-sheet";
 import { NewRoomSheet } from "./new-room-sheet";
 
 export function RoomsSheetProvider() {
   return (
     <>
       <NewRoomSheet />
+      <EditRoomSheet />
     </>
   );
 }
