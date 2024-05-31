17e4e1a953c404ac096cd398b8f07c236209151a
DanielRicra <daniel.int@outlook.com>
2024-05-31 18:28:04 -0500

    Add DeleteStudentDialog to StudentsSheetProvider

--- src/routes/_admin/admin/students/-components/students-sheet-provider.tsx
+++ src/routes/_admin/admin/students/-components/students-sheet-provider.tsx
@@ -1,9 +1,11 @@
+import { DeleteStudentDialog } from "./delete-student-dialog";
 import { NewStudentSheet } from "./new-student-sheet";
 
 export function StudentsSheetProvider() {
   return (
     <>
       <NewStudentSheet />
+      <DeleteStudentDialog />
     </>
   );
 }
