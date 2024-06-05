6ddc69f2d8e3ad28d943d3c3c69863b5ffe7a88a
DanielRicra <daniel.int@outlook.com>
2024-06-04 19:07:09 -0500

    Add useFetchClassroom

--- src/hooks/http-requests/use-fetch-classrooms.ts
+++ src/hooks/http-requests/use-fetch-classrooms.ts
@@ -3,6 +3,7 @@ import qs from "query-string";
 
 import apiService, { type HttpServiceError } from "@/services/api";
 import type { QueryString } from "@/types";
+import type { Classroom } from "@/services/types";
 
 interface FetchStudentsConfig {
   query: QueryString;
@@ -16,6 +17,13 @@ export function useFetchClassrooms<T>({
   const queryString = qs.stringify(query, { arrayFormat: "comma" });
   return useSWR<T, HttpServiceError>(
     shouldFetch ? `/classrooms?${queryString}` : null,
+    (url: string) => apiService.get(url)
+  );
+}
+
+export function useFetchClassroom<T>(classroomId?: Classroom["id"]) {
+  return useSWR<T, HttpServiceError>(
+    classroomId ? `/classrooms/${classroomId}` : null,
     (url: string) => apiService.get(url)
   );
 }
