f561cda70c88c0be0c45be63d45dff76899d98ff
DanielRicra <daniel.int@outlook.com>
2024-06-06 18:26:57 -0500

    Add students title the amount of students

--- src/routes/_admin/admin/classrooms/$classroom-id.tsx
+++ src/routes/_admin/admin/classrooms/$classroom-id.tsx
@@ -82,7 +82,7 @@ function ClassroomPage() {
           </div>
         ) : null}
 
-        <TypographyH3>Students</TypographyH3>
+        <TypographyH3>Students - {students?.length ?? 0}</TypographyH3>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
           {isLoadingStudents ? (
             <div className="space-y-4 col-span-1">
