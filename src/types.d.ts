d2a75d078f84597a01768d246481d57a5f75a455
DanielRicra <daniel.int@outlook.com>
2024-06-05 19:24:09 -0500

    Add RoomSearchQuery type

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,9 @@ export type QueryString = Record<
   string,
   string | number | undefined | string[]
 >;
+
+export type RoomSearchQuery = {
+  "capacity.gte"?: number;
+  "capacity.lte"?: number;
+  room_number?: string;
+};
