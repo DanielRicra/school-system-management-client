7f86c8a2fab24bb420d7625b7b3a301cfc823c55
DanielRicra <daniel.int@outlook.com>
2024-06-08 14:41:43 -0500

    Add useFetchTeachers and useFetchTeacher

---
+++ src/hooks/http-requests/use-fetch-teachers.ts
@@ -0,0 +1,26 @@
+import qs from "query-string";
+import useSWR from "swr";
+
+import apiService, { type HttpServiceError } from "@/services/api";
+import type { QueryString } from "@/types";
+import type { Teacher } from "@/services/types";
+
+interface FetchTeachersConfig {
+  query: QueryString;
+}
+
+export function useFetchTeachers<T>({ query }: FetchTeachersConfig) {
+  const queryString = qs.stringify(query, { arrayFormat: "comma" });
+
+  return useSWR<T, HttpServiceError>(
+    `/teachers?${queryString}`,
+    (url: string) => apiService.get(url)
+  );
+}
+
+export function useFetchTeacher<T>(teacherId?: Teacher["id"]) {
+  return useSWR<T, HttpServiceError>(
+    teacherId ? `/teachers/${teacherId}` : null,
+    (url: string) => apiService.get(url)
+  );
+}
