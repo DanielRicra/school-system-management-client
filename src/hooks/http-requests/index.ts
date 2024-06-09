b797dab2cc9b5cbf632ef062e64df78c58454e0b
DanielRicra <daniel.int@outlook.com>
2024-06-08 19:39:54 -0500

    Add useFetchUsersWithoutTeacher

--- src/hooks/http-requests/index.ts
+++ src/hooks/http-requests/index.ts
@@ -1,3 +1,5 @@
 export * from "./use-fetch-students";
 export * from "./use-fetch-classrooms";
 export * from "./use-fetch-rooms";
+export * from "./use-fetch-teachers";
+export * from "./use-fetch-users"
--- src/hooks/http-requests/use-fetch-users.ts
+++ src/hooks/http-requests/use-fetch-users.ts
@@ -11,3 +11,12 @@ export function useFetchUsersWithoutStudent<T>(fullName?: string) {
     (url: string) => apiService.get(url)
   );
 }
+
+export function useFetchUsersWithoutTeacher<T>(fullName?: string) {
+  const queryString = qs.stringify({ full_name: fullName });
+
+  return useSWR<T, HttpServiceError>(
+    `/users/without-teacher?${queryString}`,
+    (url: string) => apiService.get(url)
+  );
+}
